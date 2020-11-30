df310a9d336dab07431927e7c2f80bc9caebb149
yangej <yangej0218@gmail.com>
2020-12-01 00:12:20 +0800

    add component QuestionRow

--- src/components/common/QuestionRow.vue
+++ src/components/common/QuestionRow.vue
@@ -1,18 +1,37 @@
 <template>
-    $END$
+    <v-card max-width="800" class="shadow mb-7">
+        <div class="pa-5 pb-0">
+            <p class="font-weight-medium">{{question}}</p>
+            <v-radio-group v-model="answer">
+                <v-radio
+                    readonly
+                    v-for="(option, index) in options"
+                    :key="`option-${index}`"
+                    :label="option"
+                    :value="optionValue[index]"
+                ></v-radio>
+            </v-radio-group>
+        </div>
+        <div class="primary lighten-1 pa-5">
+            <span class="warning--text font-weight-medium">答案為 {{ answer }}，{{ analysis }}</span>
+        </div>
+    </v-card>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-
-    @Component({
-        components: {}
-    })
-    export default class QuestionRow extends Vue {
-
-    }
+<script>
+    export default {
+        name: 'QuestionRow',
+        data() {
+            return {
+                optionValue: [ 'A', 'B' ,'C' ]
+            }
+        },
+        props: {
+            tagText: String,
+            question: String,
+            answer: String,
+            options: Array,
+            analysis: String
+        }
+    };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
