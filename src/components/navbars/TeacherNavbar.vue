c543ecba54ccabb4996ef33a44a7b4eda747a1b7
yangej <yangej0218@gmail.com>
2020-12-01 00:16:05 +0800

    remove unused page

--- src/components/navbars/TeacherNavbar.vue
+++ src/components/navbars/TeacherNavbar.vue
@@ -8,7 +8,6 @@
         <v-toolbar-title class="white--text font-weight-medium">PaTrack</v-toolbar-title>
         <v-spacer/>
         <router-link class="white--text ml-15" to="/course-management">課堂管理</router-link>
-        <router-link class="white--text ml-15" to="/student-answer">答題狀況</router-link>
         <router-link class="white--text ml-15" to="/student-list">學生列表</router-link>
         <v-btn icon class="ml-10 white--text">
             <v-icon>mdi-logout</v-icon>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,11 @@ import VueRouter from "vue-router";
 import StudentLayout from "@/layout/StudentLayout";
 import TeacherLayout from "@/layout/TeacherLayout";
 import Login from "@/views/Login";
-import Register from "@/views/Register";
 import Test from "@/views/test";
 import CourseList from "@/views/student/CourseList";
 import QuestionList from "@/views/student/QuestionList";
 import CourseManagement from "@/views/teacher/CourseManagement";
-import StudentAnswer from "@/views/teacher/StudentAnswer";
+import QuestionManagement from "@/views/teacher/QuestionManagement";
 import StudentList from "@/views/teacher/StudentList";
 
 const routes = [
@@ -55,9 +54,9 @@ const routes = [
                 component: CourseManagement
             },
             {
-                path: '/student-answer',
-                name: 'StudentAnswer',
-                component: StudentAnswer
+                path: '/question-management/:id',
+                name: 'QuestionManagement',
+                component: QuestionManagement
             },
             {
                 path: '/student-list',
@@ -65,11 +64,6 @@ const routes = [
                 component: StudentList
             },
         ]
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
     },
     {
         path: '*',
