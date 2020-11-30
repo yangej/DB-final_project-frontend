5cfbe2ff324b021af082e39d26d46b66e5dbec42
yangej <yangej0218@gmail.com>
2020-11-30 15:00:24 +0800

    add temp routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,65 @@
 import VueRouter from "vue-router";
+import StudentLayout from "../layout/StudentLayout";
+import TeacherLayout from "../layout/TeacherLayout";
 import Login from "../views/Login";
 import Register from "../views/Register";
+import Test from "../views/test";
+import CourseList from "../views/student/CourseList";
+import CourseManagement from "../views/teacher/CourseManagement";
+import StudentAnswer from "../views/teacher/StudentAnswer";
+import StudentList from "../views/teacher/StudentList";
 
 const routes = [
-    {
-        path: '*',
-        redirect: '/'
-    },
     {
         path: '/',
         name: 'Login',
         component: Login
+    },
+    {
+        path: '',
+        name: 'StudentLayout',
+        component: StudentLayout,
+        redirect: '/student',
+        children: [
+            {
+                path: '/student',
+                name: 'StudentTest',
+                component: Test
+            },
+            {
+                path: '/course-list',
+                name: 'CourseList',
+                component: CourseList
+            }
+        ]
+    },
+    {
+        path: '',
+        name: 'TeacherLayout',
+        component: TeacherLayout,
+        redirect: '/teacher',
+        children: [
+            {
+                path: '/teacher',
+                name: 'TeacherTest',
+                component: Test
+            },
+            {
+                path: '/course-management',
+                name: 'CourseManagement',
+                component: CourseManagement
+            },
+            {
+                path: '/student-answer',
+                name: 'StudentAnswer',
+                component: StudentAnswer
+            },
+            {
+                path: '/student-list',
+                name: 'StudentList',
+                component: StudentList
+            },
+        ]
     },
     {
         path: '/register',
