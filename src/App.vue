051ba5f08925e0a45c7e6586926965cc6589cb01
yangej <yangej0218@gmail.com>
2020-11-30 14:59:19 +0800

    modify App

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,41 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <transition name="fade-transform" mode="out-in">
+        <router-view/>
+      </transition>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-}
+};
 </script>
 
 <style lang="scss">
-:root {
-  --main: #5BC88F;
-  --main-hover: #3A885F;
-  --bg: #efefef;
-  --shadow: rgba(191, 191, 191, 0.5) 0px 0px 10px;
-}
 * {
   transition: 0.25s;
 }
+
+.shadow {
+  box-shadow: rgba(191, 191, 191, 0.5) 0px 0px 10px !important;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-  a {
-    color: var(--main);
-    text-decoration: none;
-    cursor: pointer;
 
-    &:hover {
-      color: var(--main-hover);
-    }
-  }
-  .icon {
-    width: 126px;
-    height: 126px;
-  }
+a {
+  text-decoration: none;
+}
+
+.icon {
+  width: 126px;
+  height: 126px;
+}
 </style>
