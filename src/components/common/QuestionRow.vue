5eff4403da73b44e3e3f6cf775a8e2bea7292bf5
yangej <yangej0218@gmail.com>
2020-12-01 00:55:04 +0800

    add QuestionList and improve QuestionRow

--- src/components/common/QuestionRow.vue
+++ src/components/common/QuestionRow.vue
@@ -2,17 +2,17 @@
     <v-card max-width="800" class="shadow mb-7">
         <div class="pa-5 pb-0">
             <p class="font-weight-medium">{{question}}</p>
-            <v-radio-group v-model="answer">
+            <v-radio-group v-model="selection" @change="changeValue">
                 <v-radio
-                    readonly
                     v-for="(option, index) in options"
                     :key="`option-${index}`"
                     :label="option"
                     :value="optionValue[index]"
+                    :readonly="readonly"
                 ></v-radio>
             </v-radio-group>
         </div>
-        <div class="primary lighten-1 pa-5">
+        <div v-if="analysis" class="primary lighten-1 pa-5">
             <span class="warning--text font-weight-medium">答案為 {{ answer }}，{{ analysis }}</span>
         </div>
     </v-card>
@@ -23,15 +23,25 @@
         name: 'QuestionRow',
         data() {
             return {
-                optionValue: [ 'A', 'B' ,'C' ]
+                optionValue: [ 'A', 'B' ,'C' ],
+                selection: ''
             }
         },
         props: {
+            readonly: Boolean,
             tagText: String,
             question: String,
             answer: String,
             options: Array,
             analysis: String
+        },
+        methods: {
+            changeValue() {
+                this.$emit('update-value', this.selection);
+            }
+        },
+        mounted() {
+            this.selection = this.answer ? this.answer : '';
         }
     };
 </script>
--- src/views/student/QuestionList.vue
+++ src/views/student/QuestionList.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>Question {{ $route.params.id }}</div>
+    <back-card
+            class="mx-auto"
+            action-text="返回課堂回答"
+            @button-click="goTo('/course-list')"
+    >
+        <div class="pa-14">
+            <p class="primary--text font-weight-bold ma-0">{{ week }}</p>
+            <h3 class="headline font-weight-medium mb-7">{{ title }}</h3>
+            <div>
+                <question-row
+                    v-for="(question, index) in questions"
+                    :readonly="false"
+                    :key="`question-${index}`"
+                    :question="question.question"
+                    :options="question.options"
+                    @update-value="updateValue($event, index)"
+                ></question-row>
+            </div>
+            <custom-button
+                text="送出回答"
+                width="130"
+                :disabled="!isFinished"
+                @click="send"
+            ></custom-button>
+        </div>
+    </back-card>
 </template>
 
 <script>
+    import BackCard from "@/components/common/BackCard";
+    import QuestionRow from "@/components/common/QuestionRow";
+    import CustomButton from "@/components/common/CustomButton";
+    import { mockqQuetions } from "../../dummies/questions";
+
     export default {
         name: 'QuestionList',
+        components: { BackCard, QuestionRow, CustomButton },
+        data() {
+            return {
+                week: '',
+                title: '',
+                questions: mockqQuetions,
+                answers: [],
+            }
+        },
+        computed: {
+            isFinished() {
+                return this.answers.every((answer) => { return answer !== '' });
+            }
+        },
+        methods: {
+            goTo(path) {
+                this.$router.push(path);
+            },
+            updateValue(value, index) {
+                this.answers[index] = value;
+            },
+            send() {
+                //TODO: call api
+            }
+        },
+        mounted() {
+            this.week = `Week ${this.$route.params.id}`;
+            this.title = this.$route.params.title; // TODO: this needs api to get it
+            this.answers = Array(this.questions.length).fill('');
+        }
     };
 </script>
