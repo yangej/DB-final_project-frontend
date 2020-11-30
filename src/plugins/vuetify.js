5bdaf95b99e0b9b27383243175b48b39f66e9acc
yangej <yangej0218@gmail.com>
2020-11-30 10:11:53 +0800

    add color to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+    theme: {
+        themes: {
+            light: {
+                primary: {
+                    base: '#5BC88F',
+                    darken1: '#3A885F',
+                },
+                accent: '#efefef',
+                warning: '#E55151',
+            },
+        },
+    },
+})
