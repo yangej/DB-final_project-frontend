1fc27733d529f33d45f05ffa9997af5667ef37ce
yangej <yangej0218@gmail.com>
2020-11-30 16:51:08 +0800

    finish temp CourseList

--- src/views/student/CourseList.vue
+++ src/views/student/CourseList.vue
@@ -1,10 +1,39 @@
 <template>
-    <div>me is course list</div>
+    <div class="pt-16">
+        <main-card
+            class="mx-auto"
+            title="課堂回答"
+            en-title="Questions in Class"
+            img-src="/img/navigate.svg"
+        >
+            <template>
+                <div class="px-4">
+                    <item-list
+                        v-for="(course,index) in courseList"
+                        :key="`course-${index}`"
+                        :tag-text="course.week"
+                        :title="course.title"
+                        action-text="查看問題"
+                    ></item-list>
+                </div>
+            </template>
+        </main-card>
+    </div>
 </template>
 
 <script>
+    import MainCard from "@/components/common/MainCard";
+    import ItemList from "@/components/common/ItemList";
+    import { mockCourseList } from "@/dummies/courseList";
+
     export default {
-        name: 'CourseList'
+        name: 'CourseList',
+        components: { MainCard, ItemList },
+        data() {
+            return {
+                courseList: mockCourseList
+            };
+        }
     }
 </script>
 
