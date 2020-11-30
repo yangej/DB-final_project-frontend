bd78f1605b99f4392157fd3a124bfa31ed2e4401
yangej <yangej0218@gmail.com>
2020-12-01 00:11:04 +0800

    add color to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,6 +8,7 @@ export default new Vuetify({
         themes: {
             light: {
                 primary: {
+                    lighten1: '#F0FAF5',
                     base: '#5BC88F',
                     darken1: '#3A885F',
                 },
