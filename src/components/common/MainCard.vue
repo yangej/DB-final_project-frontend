579767b0d04f891cf4973152eb9fe6f86668e4aa
yangej <yangej0218@gmail.com>
2020-11-30 16:49:51 +0800

    add component MainCard

--- src/components/common/MainCard.vue
+++ src/components/common/MainCard.vue
@@ -1,18 +1,29 @@
 <template>
-    $END$
+    <v-card max-width="900" class="shadow pa-15">
+        <div class="header pb-10 d-flex justify-space-between align-center">
+            <div class="d-flex">
+                <img class="icon" :src="imgSrc" alt="icon">
+                <div class="ml-7 mt-6">
+                    <h3 class="headline font-weight-medium">{{ title }}</h3>
+                    <p class="font-weight-medium">{{ enTitle }}</p>
+                </div>
+            </div>
+            <slot name="button"></slot>
+        </div>
+        <slot></slot>
+    </v-card>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-
-    @Component({
-        components: {}
-    })
-    export default class MainCard extends Vue {
-
+<script>
+    export default {
+        name: 'StudentNavbar',
+        props: {
+            imgSrc: String,
+            title: String,
+            enTitle: String
+        }
     }
 </script>
-
 <style lang="scss" scoped>
 
 </style>
