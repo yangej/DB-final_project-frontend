1318ad7d4fa7569b356e60104f0ff3317279b9a7
yangej <yangej0218@gmail.com>
2020-11-30 14:52:58 +0800

    add temp StudentLayout

--- src/components/navbars/StudentNavbar.vue
+++ src/components/navbars/StudentNavbar.vue
@@ -1,18 +1,21 @@
 <template>
-    $END$
+    <v-app-bar
+        absolute
+        color="primary"
+        elevate-on-scroll
+        class="px-16"
+    >
+        <v-toolbar-title class="white--text font-weight-medium">PaTrack</v-toolbar-title>
+        <v-spacer/>
+        <router-link class="white--text" to="/course-list">課堂回答</router-link>
+        <v-btn icon class="ml-10 white--text">
+            <v-icon>mdi-logout</v-icon>
+        </v-btn>
+    </v-app-bar>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-
-    @Component({
-        components: {}
-    })
-    export default class StudentNavbar extends Vue {
-
+<script>
+    export default {
+        name: 'StudentNavbar',
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
---
+++ src/layout/StudentLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <student-navbar></student-navbar>
+        me is student layout
+        <router-view></router-view>
+    </div>
+</template>
+
+<script lang="ts">
+    import StudentNavbar from "../components/navbars/StudentNavbar.vue";
+    export default {
+        name: 'StudentLayout',
+        components: { StudentNavbar }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
