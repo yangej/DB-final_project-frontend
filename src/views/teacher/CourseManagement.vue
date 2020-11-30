a7cdd5680a3dd41bc5aee0e042bd4e85a779f199
yangej <yangej0218@gmail.com>
2020-12-01 00:10:36 +0800

    add QuestionManagement

--- src/views/teacher/CourseManagement.vue
+++ src/views/teacher/CourseManagement.vue
@@ -1,10 +1,45 @@
 <template>
-    <div>Course Management</div>
+    <div class="pt-16">
+        <main-card
+            class="mx-auto"
+            title="課程管理"
+            en-title="Course Management"
+            img-src="/img/spreadsheet.svg"
+        >
+            <template>
+                <div class="px-5">
+                    <item-row
+                        v-for="(course,index) in courseList"
+                        :key="`course-${index}`"
+                        :tag-text="course.week"
+                        :title="course.title"
+                        action-text="管理問題"
+                        @click-button="goTo('QuestionManagement', index + 1, course.title)"
+                    ></item-row>
+                </div>
+            </template>
+        </main-card>
+    </div>
 </template>
 
 <script>
+    import MainCard from "@/components/common/MainCard";
+    import ItemRow from "@/components/common/ItemRow";
+    import { mockCourseList } from "@/dummies/courseList";
+
     export default {
-        name: 'CourseManagement'
+        name: 'CourseManagement',
+        components: { MainCard, ItemRow },
+        data() {
+            return {
+                courseList: mockCourseList
+            };
+        },
+        methods: {
+            goTo(name, id, title) {
+                this.$router.push({ name, params: { id, title } });
+            }
+        }
     }
 </script>
 
--- src/views/teacher/QuestionManagement.vue
+++ src/views/teacher/QuestionManagement.vue
@@ -1,18 +1,56 @@
 <template>
-    $END$
+    <back-card
+        class="mx-auto"
+        action-text="返回課程管理"
+        @button-click="goTo('/course-management')"
+    >
+        <div class="pa-14">
+            <p class="primary--text font-weight-bold ma-0">{{ week }}</p>
+            <h3 class="headline font-weight-medium mb-7">{{ title }}</h3>
+            <div>
+                <question-row
+                    v-for="(question, index) in questions"
+                    :key="`question-${index}`"
+                    :question="question.question"
+                    :options="question.options"
+                    :answer="question.answer"
+                    :analysis="question.analysis"
+                ></question-row>
+            </div>
+            <div class="d-flex justify-end">
+                <custom-button
+                    text="送出問題"
+                    width="130"
+                ></custom-button>
+            </div>
+        </div>
+    </back-card>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
+<script>
+    import BackCard from "@/components/common/BackCard";
+    import QuestionRow from "@/components/common/QuestionRow";
+    import CustomButton from "@/components/common/CustomButton";
+    import { mockqQuetions } from "../../dummies/questions";
 
-    @Component({
-        components: {}
-    })
-    export default class QuestionManagement extends Vue {
-
-    }
+    export default {
+        name: 'QuestionManagement',
+        components: { BackCard, QuestionRow, CustomButton },
+        data() {
+            return {
+                week: '',
+                title: '',
+                questions: mockqQuetions
+            };
+        },
+        methods: {
+            goTo(path) {
+                this.$router.push(path);
+            }
+        },
+        mounted() {
+            this.week = `Week ${this.$route.params.id}`;
+            this.title = this.$route.params.title; // TODO: this needs api to get it
+        }
+    };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
