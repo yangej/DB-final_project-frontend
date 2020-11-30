24dc175c703ac7757e8affad16e3b23ec830f59b
yangej <yangej0218@gmail.com>
2020-12-01 00:11:44 +0800

    add component BackCard

--- src/components/common/BackCard.vue
+++ src/components/common/BackCard.vue
@@ -1,18 +1,30 @@
 <template>
-    $END$
+    <v-card max-width="800" class="shadow">
+        <div class="border py-5 px-8">
+            <v-btn text @click="onClick">
+                <v-icon class="primary--text mr-2">mdi-chevron-left</v-icon>
+                <span class="font-weight-medium body-1">{{ actionText }}</span>
+            </v-btn>
+        </div>
+        <slot></slot>
+    </v-card>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-
-    @Component({
-        components: {}
-    })
-    export default class BackCard extends Vue {
-
+<script>
+    export default {
+        name: 'BackCard',
+        props: {
+            actionText: String,
+        },
+        methods: {
+            onClick() {
+                this.$emit('button-click');
+            }
+        }
     }
 </script>
-
 <style lang="scss" scoped>
-
+    .border {
+        border-bottom: 1px solid #efefef;
+    }
 </style>
