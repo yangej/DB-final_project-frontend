19028dee4225621fa4898b5119c62127995e987d
yangej <yangej0218@gmail.com>
2020-11-30 16:50:27 +0800

    add component ItemList

--- src/components/common/ItemList.vue
+++ src/components/common/ItemList.vue
@@ -1,18 +1,31 @@
 <template>
-    $END$
+    <v-row class="py-3 border">
+        <v-col cols="2">
+            <span class="primary--text font-weight-medium">{{ tagText }}</span>
+        </v-col>
+        <v-col cols="8">
+            <span class=" font-weight-medium">{{ title }}</span>
+        </v-col>
+        <v-col cols="2" class="d-flex align-center">
+            <span class="text-right font-weight-medium">{{ actionText }}</span>
+            <v-icon class="primary--text ml-2">mdi-chevron-right</v-icon>
+        </v-col>
+    </v-row>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-
-    @Component({
-        components: {}
-    })
-    export default class ItemList extends Vue {
-
+<script>
+    export default {
+        name: 'ItemList',
+        props: {
+            tagText: String,
+            title: String,
+            actionText: String
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+.border {
+    border-bottom: 1px solid #efefef;
+}
 </style>
