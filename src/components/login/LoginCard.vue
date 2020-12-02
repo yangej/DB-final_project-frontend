dd90bbb8c5fa52190b9f3a95486518e3e43cb113
yangej <yangej0218@gmail.com>
2020-12-02 21:24:51 +0800

    Resolve #9 Modify login page (#10)
    * modify file paths
    
    * modify login page
    
    * add temp login method

--- src/components/login/LoginCard.vue
+++ src/components/login/LoginCard.vue
@@ -1,14 +1,14 @@
 <template>
     <v-card min-width="400" class="card shadow py-16 px-10">
-        <div class="d-flex mb-10">
-            <img class="icon" src="/img/login.svg" alt="login-icon">
-            <div class="mt-6 pl-5 text-left">
-                <h3 class="display-1 mb-3 font-weight-medium">登入</h3>
-                <p>請輸入您的帳號與密碼</p>
+        <div class="d-flex flex-column align-center mb-5">
+            <div class="logo primary d-flex justify-center align-center rounded-circle mb-5">
+                <img src="/img/logo.svg" alt="logo">
             </div>
+            <h3 class="display-1 mb-3 font-weight-medium primary--text">PaTrack</h3>
         </div>
         <div>
             <v-text-field
+                v-model="account"
                 label="Outlined"
                 hide-details
                 single-line
@@ -17,6 +17,7 @@
                 placeholder="請輸入您的帳號"
             ></v-text-field>
             <v-text-field
+                v-model="password"
                 label="Outlined"
                 hide-details
                 type="password"
@@ -25,20 +26,25 @@
                 class="mb-10"
                 placeholder="請輸入您的密碼"
             ></v-text-field>
-            <custom-button width="100%" text="登入"></custom-button>
-            <a class="text-center d-block mt-7 font-weight-medium" @click="goTo('/register')">我要註冊</a>
+            <custom-button width="100%" text="登入" @click="login"></custom-button>
         </div>
     </v-card>
 </template>
 
 <script>
-import CustomButton from "./common/CustomButton";
+import CustomButton from "@/components/common/CustomButton";
 export default {
     name: 'LoginCard',
     components: { CustomButton },
+    data() {
+        return {
+            account: '',
+            password: ''
+        }
+    },
     methods: {
-        goTo (path) {
-            this.$router.push(path);
+        login() {
+            this.$emit('login', { account: this.account, password: this.password })
         }
     }
 }
@@ -48,5 +54,14 @@ export default {
     ::v-deep fieldset {
         border: none !important;
         background-color: #efefef;
+    }
+    .logo {
+        width: 100px;
+        height: 100px;
+
+        img {
+            width: 64px;
+            height: 64px;
+        }
     }
 </style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container d-flex justify-center align-center py-16">
+        <login-card
+            @login="login"
+        ></login-card>
+    </div>
+</template>
+
+<script>
+    import LoginCard from '@/components/login/LoginCard.vue'
+
+    export default {
+        name: 'Login',
+        components: {
+            LoginCard
+        },
+        methods: {
+            login(values) {
+                // TODO: call api and login
+                console.log(values.account, values.password);
+            }
+        }
+    }
+</script>
