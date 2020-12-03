4786f514e73fb49a515d4d4365231dc91a3e7e9c
yangej <yangej0218@gmail.com>
2020-12-03 23:21:36 +0800

    Resolve #13 Create component Popup (#14)
    * create component Popup
    
    * add component Popup to App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
     <v-main class="accent">
+      <popup
+        :show-popup="popup.showPopup"
+        :img-src="popup.imgSrc"
+        :popup-text="popup.popupText"
+        @close-popup="closePopup"
+      ></popup>
       <transition name="fade" mode="out-in">
         <router-view/>
       </transition>
@@ -9,9 +15,22 @@
 </template>
 
 <script>
+import Popup from '@/components/common/Popup';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'App',
+  components: { Popup },
+  computed: {
+    ...mapGetters({
+      popup: 'popup/options'
+    })
+  },
+  methods: {
+    ...mapActions({
+      closePopup: 'popup/closePopup'
+    })
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from 'vuex';
+import { popup } from "./modules/popup";
 
 Vue.use(Vuex);
+
 const store = new Vuex.Store({
-    state: {
-        test: 'test'
-    }
+    modules: { popup }
 });
 
 export default store;
