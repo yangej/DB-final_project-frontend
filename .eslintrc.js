1ebbfa90798320256f90597a5ce167160a26dd49
yangej <yangej0218@gmail.com>
2020-12-12 01:22:43 +0800

    set eslint parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    parser: 'babel-eslint',
+    parser: 'vue-eslint-parser',
 
     parserOptions: {
         ecmaVersion: 2017,
         sourceType: 'module',
+        parser: 'babel-eslint',
     },
 
     rules: {
         'no-unused-vars': [
             'error',
-            { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+            { vars: 'all', args: 'none', ignoreRestSiblings: false },
         ],
     },
 };
