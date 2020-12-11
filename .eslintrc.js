f023c9b4b5c9726dd436cb4eec5de424be21c37a
yangej <yangej0218@gmail.com>
2020-12-12 00:59:29 +0800

    add eslint rule

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        'no-unused-vars': [
+            'error',
+            { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+        ],
+    },
+};
