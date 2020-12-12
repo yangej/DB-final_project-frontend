5ef943c344c7180f957fe4c62e452596cd41c6c1
yangej <yangej0218@gmail.com>
2020-12-13 01:01:34 +0800

    add temp StudentList

--- src/views/teacher/StudentList.vue
+++ src/views/teacher/StudentList.vue
@@ -1,11 +1,120 @@
 <template>
-    <div>Student List</div>
+    <div class="pt-16">
+        <main-card
+            class="mx-auto"
+            title="學生列表"
+            en-title="Student List"
+            img-src="/img/search.svg"
+        >
+            <template>
+                <div class="px-5">
+                    <v-data-table
+                        :headers="headers"
+                        :items="students"
+                        :sort-by="'avgScore'"
+                        :sort-desc="false"
+                        multi-sort
+                        class="shadow"
+                        hide-default-footer
+                        :page.sync="page"
+                        :items-per-page="itemsPerPage"
+                        @page-count="pageCount = $event"
+                    >
+                        <template v-slot:header.action="{ header }">
+                            <span class="pl-4">{{ header.text }}</span>
+                        </template>
+                        <template v-slot:item.action="{ item }">
+                            <v-btn text @click="onClick(item.id)">
+                                <span class="font-weight-medium body-1"
+                                    >查看狀況</span
+                                >
+                                <v-icon class="primary--text ml-2"
+                                    >mdi-chevron-right</v-icon
+                                >
+                            </v-btn>
+                        </template>
+                    </v-data-table>
+                </div>
+            </template>
+            <v-pagination
+                v-model="page"
+                class="mt-6"
+                :length="pageCount"
+            ></v-pagination>
+        </main-card>
+    </div>
 </template>
 
 <script>
+import MainCard from '@/components/common/MainCard';
+import { mockStudents } from '@/dummies/students';
+
 export default {
     name: 'StudentList',
+    components: { MainCard },
+    data() {
+        return {
+            page: 1,
+            pageCount: 0,
+            itemsPerPage: 5,
+            headers: [
+                { text: '學號', value: 'id' },
+                { text: '姓名', value: 'name', sortable: false },
+                { text: '系所', value: 'department', sortable: false },
+                { text: '平均分數', value: 'avgScore' },
+                { text: '操作', value: 'action', sortable: false },
+            ],
+            students: mockStudents,
+        };
+    },
+    methods: {
+        onClick(id) {
+            //TODO: route to detail page
+            console.log('clicked', id);
+        },
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::v-deep .v-pagination {
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 10px;
+
+    button {
+        box-shadow: rgba(191, 191, 191, 0.7) 0px 3px 6px !important;
+    }
+}
+
+::v-deep table {
+    border-top-left-radius: 5px !important;
+    border-top-right-radius: 5px !important;
+    overflow: hidden;
+}
+
+::v-deep .v-data-table-header {
+    background-color: #5bc88f;
+
+    th {
+        padding: 24px 20px !important;
+        border-bottom: 1px solid #efefef !important;
+        span,
+        i {
+            font-size: 16px;
+            color: white !important;
+        }
+    }
+
+    i {
+        margin: 0 4px;
+    }
+}
+
+::v-deep td {
+    border-bottom: 1px solid #efefef !important;
+    padding: 16px 20px !important;
+    font-size: 16px !important;
+    font-weight: 500;
+}
+</style>
