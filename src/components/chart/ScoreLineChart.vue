b9b281a766999c174f0ddde28a1a177574cd97bf
yangej <yangej0218@gmail.com>
2020-12-13 12:16:30 +0800

    create component ScoreLineChart

---
+++ src/components/chart/ScoreLineChart.vue
@@ -0,0 +1,42 @@
+<template>
+    <ve-chart
+        :data="data"
+        :legend-visible="false"
+        :extend="chartSettings"
+    ></ve-chart>
+</template>
+
+<script>
+import VeChart from 'v-charts/lib/line';
+import 'echarts/lib/component/markLine';
+
+export default {
+    name: 'ScoreLineChart',
+    components: { VeChart },
+    props: {
+        data: Object,
+    },
+    data() {
+        return {
+            chartSettings: {
+                color: ['#5BC88F'],
+                series: {
+                    markLine: {
+                        data: [
+                            {
+                                type: 'average',
+                            },
+                        ],
+                        silent: true,
+                        label: {
+                            show: false,
+                        },
+                    },
+                },
+            },
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
