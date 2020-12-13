7cfa177a80fb6288430eb8040d53729bb79216e4
yangej <yangej0218@gmail.com>
2020-12-13 12:12:37 +0800

    add and modify dummies

---
+++ src/dummies/scores.js
@@ -0,0 +1,152 @@
+export const mockScores = [
+    {
+        studentId: 109462001,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462002,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109753133,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462004,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462005,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109753136,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462007,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462008,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109753139,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462000,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462011,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109753135,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462012,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109462013,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+    {
+        studentId: 109753137,
+        scores: [
+            { unit: 'Unit 1', title: '資料庫介紹', score: 90 },
+            { unit: 'Unit 2', title: '資料庫系統概覽', score: 80 },
+            { unit: 'Unit 3', title: '關連式資料庫設計', score: 70 },
+            { unit: 'Unit 4', title: 'SQL基本指令 1', score: 50 },
+            { unit: 'Unit 5', title: 'SQL基本指令 2', score: 90 },
+        ],
+    },
+];
