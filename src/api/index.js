d1727aa67a6adc38dbff67661f34bbafffd43d51
yangej <yangej0218@gmail.com>
2020-12-14 15:09:51 +0800

    set real api infos

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,41 @@
 import axios from 'axios';
+import { setInterceptor } from './interceptor';
 
 const apiFactory = function (axios) {
     return {
-        getAllCourses() {
-            return axios.get('/courses');
+        getAllUnits() {
+            return axios.get('/units');
         },
-        getQuestion(id) {
-            return axios.get(`/questions/${id}`);
+        // getQuestion(id) {
+        //     return axios.get(`/questions/${id}`);
+        // },
+        // setQuestionSent(id, isSent) {
+        //     return axios.post(`/send-questions/${id}`, isSent);
+        // },
+        // getAllStudents() {
+        //     return axios.get('/students');
+        // },
+        // getStudent(id) {
+        //     return axios.get(`/students/${id}`);
+        // },
+        // getScores(id) {
+        //     return axios.get(`/scores/${id}`);
+        // },
+        login(loginInfos) {
+            return axios.post('/login', loginInfos);
         },
-        setQuestionSent(id, isSent) {
-            return axios.post(`/send-questions/${id}`, isSent);
-        },
-        getAllStudents() {
-            return axios.get('/students');
-        },
-        getStudent(id) {
-            return axios.get(`/students/${id}`);
-        },
-        getScores(id) {
-            return axios.get(`/scores/${id}`);
-        },
-        login() {
-            return axios.get('/login');
-        },
-        submitAnswers(answerInfos) {
-            return axios.post('/answers', answerInfos);
-        },
+        // submitAnswers(answerInfos) {
+        //     return axios.post('/answers', answerInfos);
+        // },
     };
 };
 
 const config = {
-    baseURL: '/api',
-    headers: { 'Content-Type': 'application/json' },
+    baseURL: 'http://localhost:5000',
+    'Content-Type': 'application/json',
 };
 
 const axiosInstance = axios.create(config);
+setInterceptor(axiosInstance);
 
 export const apiExecutor = apiFactory(axiosInstance);
