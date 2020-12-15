ba9fa292308dc4eb838b35c6edb12669ada3b468
yangej <yangej0218@gmail.com>
2020-12-15 14:30:24 +0800

    add mock data of student scores

--- src/dummies/summaryData.js
+++ src/dummies/summaryData.js
@@ -37,107 +37,141 @@ export const mockQuestionOverview = {
     ],
 };
 
-export const mockQuestionResults = [
+export const mockQuestionResults = {
+    unitTitle: '資料庫介紹',
+    questions: [
+        {
+            question: '請問下列何者與其他二者不同？',
+            answer: 'C',
+            option_a: '資料庫系統',
+            option_b: '資料庫管理系統',
+            option_c: '資料庫',
+            option_d: '資料庫程式語言',
+            studentAnswers: [
+                {
+                    name: '楊伊捷',
+                    option: 'A',
+                },
+                {
+                    name: '楊伊捷',
+                    option: 'B',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'A',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+            ],
+        },
+        {
+            question: '請問下列何者與其他二者不同？',
+            answer: 'C',
+            option_a: '資料庫系統',
+            option_b: '資料庫管理系統',
+            option_c: '資料庫',
+            option_d: '資料庫程式語言',
+            studentAnswers: [
+                {
+                    name: '楊伊捷',
+                    option: 'A',
+                },
+                {
+                    name: '楊伊捷',
+                    option: 'B',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'A',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+            ],
+        },
+        {
+            question: '請問下列何者與其他二者不同？',
+            answer: 'C',
+            option_a: '資料庫系統',
+            option_b: '資料庫管理系統',
+            option_c: '資料庫',
+            option_d: '資料庫程式語言',
+            studentAnswers: [
+                {
+                    name: '楊伊捷',
+                    option: 'A',
+                },
+                {
+                    name: '楊伊捷',
+                    option: 'B',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'A',
+                },
+                {
+                    name: '王韻淇',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+                {
+                    name: '林雅雯',
+                    option: 'C',
+                },
+            ],
+        },
+    ],
+};
+
+export const mockStudentScores = [
     {
-        question: '請問下列何者與其他二者不同？',
-        answer: 'C',
-        option_a: '資料庫系統',
-        option_b: '資料庫管理系統',
-        option_c: '資料庫',
-        option_d: '資料庫程式語言',
-        studentAnswers: [
-            {
-                name: '楊伊捷',
-                option: 'A',
-            },
-            {
-                name: '楊伊捷',
-                option: 'B',
-            },
-            {
-                name: '王韻淇',
-                option: 'A',
-            },
-            {
-                name: '王韻淇',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-        ],
+        name: '楊伊捷',
+        score: 90,
     },
     {
-        question: '請問下列何者與其他二者不同？',
-        answer: 'C',
-        option_a: '資料庫系統',
-        option_b: '資料庫管理系統',
-        option_c: '資料庫',
-        option_d: '資料庫程式語言',
-        studentAnswers: [
-            {
-                name: '楊伊捷',
-                option: 'A',
-            },
-            {
-                name: '楊伊捷',
-                option: 'B',
-            },
-            {
-                name: '王韻淇',
-                option: 'A',
-            },
-            {
-                name: '王韻淇',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-        ],
+        name: '林雅雯',
+        score: 80,
     },
     {
-        question: '請問下列何者與其他二者不同？',
-        answer: 'C',
-        option_a: '資料庫系統',
-        option_b: '資料庫管理系統',
-        option_c: '資料庫',
-        option_d: '資料庫程式語言',
-        studentAnswers: [
-            {
-                name: '楊伊捷',
-                option: 'A',
-            },
-            {
-                name: '楊伊捷',
-                option: 'B',
-            },
-            {
-                name: '王韻淇',
-                option: 'A',
-            },
-            {
-                name: '王韻淇',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-            {
-                name: '林雅雯',
-                option: 'C',
-            },
-        ],
+        name: '王韻淇',
+        score: 70,
+    },
+    {
+        name: '林雅雯',
+        score: 50,
+    },
+    {
+        name: '王韻淇',
+        score: 60,
+    },
+    {
+        name: '楊伊捷',
+        score: 70,
+    },
+    {
+        name: '林雅雯',
+        score: 90,
     },
 ];
