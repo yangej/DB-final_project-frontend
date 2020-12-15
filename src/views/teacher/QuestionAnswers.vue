2e3af8355ed44aa965e8d41a442cf772baffaa42
yangej <yangej0218@gmail.com>
2020-12-15 19:19:01 +0800

    add quetion charts section

--- src/views/teacher/QuestionAnswers.vue
+++ src/views/teacher/QuestionAnswers.vue
@@ -7,7 +7,25 @@
         <div class="pa-14">
             <unit-title :title="title" :unit="unit"></unit-title>
             <div>
-                <score-bar-chart :data="overallChartData"></score-bar-chart>
+                <score-histogram :data="overallChartData"></score-histogram>
+            </div>
+            <div>
+                <h3 class="headline font-weight-medium mb-7">各題狀況</h3>
+                <question-row
+                    v-for="(question, index) in questions"
+                    :readonly="true"
+                    :key="`question-${index}`"
+                    :question="question.question"
+                    :options="question.options"
+                    :answer="question.answer"
+                >
+                    <div class="mt-n10">
+                        <score-bar-chart
+                            class="px-10"
+                            :dataset="questionChartDatas[index]"
+                        ></score-bar-chart>
+                    </div>
+                </question-row>
             </div>
         </div>
     </back-card>
@@ -16,7 +34,10 @@
 <script>
 import BackCard from '@/components/common/BackCard';
 import UnitTitle from '@/components/common/UnitTitle';
+import ScoreHistogram from '@/components/chart/ScoreHistogram';
 import ScoreBarChart from '@/components/chart/ScoreBarChart';
+import QuestionRow from '@/components/common/QuestionRow';
+
 import {
     mockQuestionResults,
     mockStudentScores,
@@ -24,7 +45,13 @@ import {
 
 export default {
     name: 'QuestionAnswers',
-    components: { BackCard, UnitTitle, ScoreBarChart },
+    components: {
+        BackCard,
+        UnitTitle,
+        ScoreHistogram,
+        ScoreBarChart,
+        QuestionRow,
+    },
     data() {
         return {
             title: '',
@@ -33,6 +60,8 @@ export default {
                 columns: ['name', 'score'],
                 rows: [],
             },
+            questionChartDatas: [],
+            questions: [],
         };
     },
     computed: {
@@ -44,11 +73,81 @@ export default {
         goTo(path) {
             this.$router.push({ path });
         },
+        resetQuestion(question) {
+            const options = [
+                `A ${question.option_a}`,
+                `B ${question.option_b}`,
+                `C ${question.option_c}`,
+                `D ${question.option_d}`,
+            ];
+            const countAnswers = this.countAnswers(question);
+            return {
+                question: question.question,
+                answer: question.answer,
+                countAnswers,
+                options,
+            };
+        },
+        countAnswers(question) {
+            const answerCount = {
+                A: { count: 0, students: [] },
+                B: { count: 0, students: [] },
+                C: { count: 0, students: [] },
+                D: { count: 0, students: [] },
+            };
+            question.studentAnswers.forEach((studentAnswer) => {
+                const option = studentAnswer.option;
+                answerCount[option].count += 1;
+                answerCount[option].students.push(studentAnswer.name);
+            });
+            return answerCount;
+        },
+        formatQuestions(questions) {
+            return questions.map((question) => {
+                return this.resetQuestion(question);
+            });
+        },
+        getQuestionRows(questions) {
+            return questions.map((question) => {
+                const A = question.countAnswers['A'];
+                const B = question.countAnswers['B'];
+                const C = question.countAnswers['C'];
+                const D = question.countAnswers['D'];
+                return {
+                    columns: ['option', 'count', 'students'],
+                    rows: [
+                        {
+                            option: 'A',
+                            count: A.count,
+                            students: A.students.join('、'),
+                        },
+                        {
+                            option: 'B',
+                            count: B.count,
+                            students: B.students.join('、'),
+                        },
+                        {
+                            option: 'C',
+                            count: C.count,
+                            students: C.students.join('、'),
+                        },
+                        {
+                            option: 'D',
+                            count: D.count,
+                            students: D.students.join('、'),
+                        },
+                    ],
+                };
+            });
+        },
     },
     mounted() {
         const resultRes = mockQuestionResults;
         const studentRes = mockStudentScores;
 
+        this.questions = this.formatQuestions(resultRes.questions);
+
+        this.questionChartDatas = this.getQuestionRows(this.questions);
         this.overallChartData = { ...this.overallChartData, rows: studentRes };
         this.title = resultRes.unitTitle;
         this.unitId = this.$route.params.id;
